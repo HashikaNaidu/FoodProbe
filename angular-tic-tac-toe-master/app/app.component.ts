3f80d50296b5a10eabe709f8a8a7f260f839e8fc
Nikhitha ch <nikhithachinthakindi@gmail.com>
2017-06-07 09:15:31 +0530

    updated

--- angular-tic-tac-toe-master/app/app.component.ts
+++ angular-tic-tac-toe-master/app/app.component.ts
@@ -5,25 +5,31 @@ import game from './game';
     selector: 'my-app',
     template: `
     
- <center>  <h1>Tic Tac Toe</h1> </center>
+    <center><h1 style = "font-family: Comic Sans MS"><a href = "#"> <b>TIC-TAC-TOE</b> </a></h1>
+    
+   <center><h1> <span *ngIf="turn==0" >second Player turn</span></h1> </center>
+   <center><h1> <span *ngIf="turn==1"> first player turn</span></h1> </center>       
     <div class="board">
         <div *ngFor="#row of board;#rowNo=index" class="row">
             <div *ngFor="#cell of row;#colNo=index" class="cell" (click)="cellClicked(rowNo, colNo)">
-                <span class="marker" *ngIf="cell===0">o</span>
-                <span class="marker" *ngIf="cell===1">x</span>
-                
+                <span class="marker" *ngIf="cell===0">X</span>
+                <span class="marker" *ngIf="cell===1">O</span>
             </div>
         </div>
     </div>
-    
-
-    <button class="restart" (click)="restart()">Play Again</button>`
+     <br><br>
+    <div class="gameResults" *ngIf="winner===0">Hurray ! First Player WON 👏</div>
+    <div class="gameResults" *ngIf="winner===1" >Hurray ! Second Player WON 👏</div>
+    <div class = "gameResults" *ngIf = "winner  ===2">Draw</div>
+     <button class="restart" (click)="restart()">Play Again</button>`
 })
 
 export class AppComponent {
     public currentPlayer = 0;
     public board = game.createBoard();
     public winner;
+    public draw ;
+    public turn:any = 1;
 
     restart() {
         this.board = game.createBoard();
@@ -32,12 +38,23 @@ export class AppComponent {
     }
 
     cellClicked(row, col) {
-        if (this.board[row][col] === 0 || this.board[row][col] === 1 || this.winner != null)
+        if (this.board[row][col] === 0 || this.board[row][col] === 1 || this.winner != null){
+            
+            
             return;
-
+        } 
         
+        this.currentPlayer++;
         this.board[row][col] = this.currentPlayer % 2 ? 0 : 1;
-
-        
+        this.turn = this.board[row][col];
+        var checkResult = game.checkBoard(this.board);
+        console.log(checkResult);
+        if (checkResult) {
+            this.winner = checkResult.winner;
+            this.turn = 2;
+        }
+        if(checkResult == 2){
+            this.winner = 2;
         }
     }
+}
