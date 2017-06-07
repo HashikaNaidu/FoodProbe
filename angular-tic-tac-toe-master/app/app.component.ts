9d4fc6c36f58745efb6498f0b03169e17032cd5f
Hashika Naidu <hashika.ammu@gmail.com>
2017-06-07 10:51:32 +0530

    updated

--- angular-tic-tac-toe-master/app/app.component.ts
+++ angular-tic-tac-toe-master/app/app.component.ts
@@ -1,14 +1,15 @@
-import {Component} from 'angular2/core';
+
+ import {Component} from 'angular2/core';
 import game from './game';
 
 @Component({
     selector: 'my-app',
     template: `
     
-    <center><h1 style = "font-family: Comic Sans MS"><a href = "#"> <b>TIC-TAC-TOE</b> </a></h1>
-    
-   <center><h1> <span *ngIf="turn==0" >second Player turn</span></h1> </center>
-   <center><h1> <span *ngIf="turn==1"> first player turn</span></h1> </center>       
+    <center><h1 style="font-family:Comic Sans MS" > <b>TIC-TAC-TOE</b></h1></center>
+    <div> <center><span *ngIf="turn==1"> Turn of <h3><b><font color="black">Player1</font></b></h3></span></center>
+     <center><span *ngIf="turn==0"> Turn of <h3><b><font color="black">Player2</font></b></h3></span></center></div>
+        
     <div class="board">
         <div *ngFor="#row of board;#rowNo=index" class="row">
             <div *ngFor="#cell of row;#colNo=index" class="cell" (click)="cellClicked(rowNo, colNo)">
@@ -17,19 +18,19 @@ import game from './game';
             </div>
         </div>
     </div>
-     <br><br>
-    <div class="gameResults" *ngIf="winner===0">Hurray ! First Player WON 👏</div>
-    <div class="gameResults" *ngIf="winner===1" >Hurray ! Second Player WON 👏</div>
-    <div class = "gameResults" *ngIf = "winner  ===2">Draw</div>
-     <button class="restart" (click)="restart()">Play Again</button>`
+    
+    <div class="gameResults" *ngIf="winner===0"><font color = "darkgreen"> Hurray ! First Player WON 👏</font></div>
+    <div class="gameResults" *ngIf="winner===1"><font color = "darkgreen">Hurray ! Second Player WON 👏</font></div>
+     <div class="gameResults" *ngIf="winner===2"><font color = "red">Game Draw</font> </div>
+    <button class="restart" (click)="restart()"><b>Restart</b></button>`
+
 })
 
 export class AppComponent {
     public currentPlayer = 0;
     public board = game.createBoard();
     public winner;
-    public draw ;
-    public turn:any = 1;
+    public turn:any = 1 ;
 
     restart() {
         this.board = game.createBoard();
@@ -39,22 +40,20 @@ export class AppComponent {
 
     cellClicked(row, col) {
         if (this.board[row][col] === 0 || this.board[row][col] === 1 || this.winner != null){
-            
-            
             return;
-        } 
+        }
         
         this.currentPlayer++;
         this.board[row][col] = this.currentPlayer % 2 ? 0 : 1;
         this.turn = this.board[row][col];
         var checkResult = game.checkBoard(this.board);
-        console.log(checkResult);
+
         if (checkResult) {
             this.winner = checkResult.winner;
             this.turn = 2;
         }
-        if(checkResult == 2){
-            this.winner = 2;
+if(checkResult == 2){
+this.winner = 2;
         }
     }
 }
